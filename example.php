a768b61d75fa803dd4237beb5ecdd4f8f26bd151
Suppanutpw <54876422+Suppanutpw@users.noreply.github.com>
2020-10-18 15:22:09 +0700

    Create example.php

---
+++ example.php
@@ -0,0 +1,16 @@
+<?php
+  require('header.php');
+?>
+  <!-- รูปด้านบน ถัดจาก menubar สีเทาด้านบน -->
+  <div>
+
+  </div>
+
+  <!-- เนื้อหา -->
+  <div class="container">
+    <h2>Content Here</h2>
+  </div>
+
+<?php
+  require('footer.php');
+?>
