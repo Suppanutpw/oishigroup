a9eb42976bd001631cb3d6980b82bef300824e5e
Pitawas <62070260@it.kmitl.ac.th>
2020-11-05 19:32:53 +0700

    Update export_center.php

--- export_center.php
+++ export_center.php
@@ -240,7 +240,7 @@
                 display += `<div id="popup-point${i}" data-effect="mfp-zoom-in" class="white-popup mfp-with-anim mfp-hide clearfix"><div class="popup-text-pic">`
                 display += `<img class="img-fluid" src="${data2[i].img}"></div></div>`
             }
-            body.innerHTML += display
+            document.getElementIdClass('content').innerHTML += display
         }
     </script>
     <script>
