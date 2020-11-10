561791eb03e7f66942961c810c3bbca864c55ac1
Suppanutpw <54876422+Suppanutpw@users.noreply.github.com>
2020-11-10 18:24:12 +0700

    Update popuptext.php

--- popuptext.php
+++ popuptext.php
@@ -43,7 +43,7 @@
     <div class="popup-text-pic">
       <img class="img-fluid" src="https://www.thairath.co.th/media/dFQROr7oWzulq5FZWt5uOWxNqVgnUIRnnhFngXa2ttHqDnSclT4eKMvFn6UPjnX1dZU.jpg">
       <div class="mt-4">
-        <h1>สวัสดีจ้า มาปรับทัศนคติกันเถอะ!!</h1>
+        <h1>สวัสดีจ้า มาปรับทัศนคติกันเถอะ1!!</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus laboriosam odio voluptates qui nemo animi, officiis, maxime quisquam veritatis sint necessitatibus illo velit. Consectetur recusandae tempore consequatur sed nostrum mollitia nesciunt voluptatum eius accusantium magnam, a, similique voluptas dignissimos esse pariatur asperiores rem. Iste inventore voluptas est, repellendus similique provident?</p>
       </div>
     </div>
@@ -54,7 +54,7 @@
     <div class="popup-text-pic">
       <img class="img-fluid" src="https://www.thairath.co.th/media/dFQROr7oWzulq5FZWt5uOWxNqVgnUIRnnhFngXa2ttHqDnSclT4eKMvFn6UPjnX1dZU.jpg">
       <div class="mt-4">
-        <h1>สวัสดีจ้า มาปรับทัศนคติกันเถอะ!!</h1>
+        <h1>สวัสดีจ้า มาปรับทัศนคติกันเถอะ2!!</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus laboriosam odio voluptates qui nemo animi, officiis, maxime quisquam veritatis sint necessitatibus illo velit. Consectetur recusandae tempore consequatur sed nostrum mollitia nesciunt voluptatum eius accusantium magnam, a, similique voluptas dignissimos esse pariatur asperiores rem. Iste inventore voluptas est, repellendus similique provident?</p>
       </div>
     </div>
